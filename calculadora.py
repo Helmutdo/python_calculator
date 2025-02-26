4932b2eb4f6dd882c7df412d36116762c6c9c9e1
Helmutdo <helmut.schweitzerg@gmail.com>
2025-02-25 23:36:49 -0300

    math operations added

--- calculadora.py
+++ calculadora.py
@@ -4,3 +4,22 @@ print(separator,"\n")
 number_first = float(input("input a number: "))
 number_second = float(input("input another number: "))
 
+operation = input("""Write the desired operation: """)
+operation = operation.lower()
+
+result = 0
+
+if operation == "sum":
+    result = number_first + number_second
+elif operation == "minus":
+    result = number_first - number_second
+elif operation == "multiplication":
+    result = number_first * number_second
+elif operation == "division":
+    try:
+        result = number_first / number_second
+    except ZeroDivisionError:
+        print("Error: division by zero.")
+        result = "indefined"
+
+print("your result is:",result)
